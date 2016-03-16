38aae1c81cd54bf70461b5b3bdc3e379357a87a3
Morgancentral99 <morgancentral99@gmail.com>
2016-03-16 12:49:31 -0600

    added Version Check

--- src/main/java/hexeption/morgancentral99/Main.java
+++ src/main/java/hexeption/morgancentral99/Main.java
@@ -5,10 +5,17 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.weather.WeatherChangeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.Bentipa.updater.SpigotPluginUpdater;
+
 public class Main extends JavaPlugin implements Listener {
 	public void onEnable() {
 		getServer().getConsoleSender().sendMessage("No Rain V0.0.1 Loaded");
         getServer().getPluginManager().registerEvents(this, this);
+        SpigotPluginUpdater spu = new SpigotPluginUpdater(this/*Your Plugin*/, "http://http://localhost/VersionChecker/hNoRain.html");
+        spu.enableOut(); // Enables an Output if there is a new Update and if the file was downloaded
+       //spu.disableOut(); Disables the output
+        if(spu.needsUpdate()) // Check if there is an Update availible
+        //...
 	}
 	@EventHandler
 	public void onWeatherChange(WeatherChangeEvent event){
